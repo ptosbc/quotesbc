5285b45b71b0bfa42544dd4d16f90f65730fa9bb
PTOS B. C <ptosbc@gmail.com>
2023-11-18 11:18:08 +0530

    verify google search console

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PTOS B. C.",
-  description: "Hii, I'm PTOS B. C.",
+  title: "Quotes B. C.",
+  description: "Get a random quote.",
 };
 
 export default function RootLayout({
